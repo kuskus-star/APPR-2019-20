4567829dfc046c1c13eca6ca24a2b80087f75b67
kuskus-star <casvalgrandma@gmail.com>
2020-09-30 16:59:35 +0200

    Prevedel gesla v slovenščino

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -12,16 +12,20 @@ Neudelezevanje$UNIT <- NULL
 Neudelezevanje$DEG_URB<-NULL
 Neudelezevanje$HHTYP<-NULL
 Neudelezevanje$FREQUENC<-NULL
-Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "First quintile"] <- 1
-Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "Second quintile"] <- 2
-Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "Third quintile"] <- 3
-Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "Fourth quintile"] <- 4
-Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "Fifth quintile"] <- 5
+Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "First quintile"] <- "1."
+Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "Second quintile"] <- "2."
+Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "Third quintile"] <- "3."
+Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "Fourth quintile"] <- "4."
+Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "Fifth quintile"] <- "5."
+Neudelezevanje$QUANTILE[Neudelezevanje$QUANTILE == "Total"] <- "Celotna populacija"
 Neudelezevanje$TIME[Neudelezevanje$TIME == 2006] <- "2006"
 Neudelezevanje$TIME[Neudelezevanje$TIME == 2015] <- "2015"
-Neudelezevanje$ACL00[Neudelezevanje$ACL00 == "Live performances (theatre, concerts, ballet)"] <- "Live performances"
-Neudelezevanje$ACL00[Neudelezevanje$ACL00 == "Cultural activities (cinema, live performances or cultural sites)"] <- "Cultural activities"
-Neudelezevanje$ACL00[Neudelezevanje$ACL00 == "Cultural sites (historical monuments, museums, art galleries or archaeological sites)"] <- "Cultural sites"
+Neudelezevanje$ACL00[Neudelezevanje$ACL00 == "Live performances (theatre, concerts, ballet)"] <- "Nastopi v živo"
+Neudelezevanje$ACL00[Neudelezevanje$ACL00 == "Cultural activities (cinema, live performances or cultural sites)"] <- "Kulturne Aktivnosti"
+Neudelezevanje$ACL00[Neudelezevanje$ACL00 == "Cultural sites (historical monuments, museums, art galleries or archaeological sites)"] <- "Muzeji in galerije"
+Neudelezevanje$ACL00[Neudelezevanje$ACL00 == "Live performances (theatre, concerts, ballet)"] <- "Nastopi v živo"
+Neudelezevanje$ACL00[Neudelezevanje$ACL00 == "Cinema"] <- "Kino"
+Neudelezevanje$ACL00[Neudelezevanje$ACL00 == "Sports events"] <- "Športni dogodki"
 Neudelezevanje_6 <-filter(Neudelezevanje,Neudelezevanje$TIME =="2006")
 Neudelezevanje_6$TIME <- NULL
 Neudelezevanje_15 <-filter(Neudelezevanje,Neudelezevanje$TIME =="2015")
@@ -32,18 +36,22 @@ Razlogi <- read_csv("podatki/ilc_scp06_1_Data.csv", col_names=TRUE, na=":" ,loca
 Razlogi$GEO <- standardize.countrynames(Razlogi$GEO,suggest = "auto", print.changes = TRUE)
 a
 names(Razlogi)[10]<-"FnF"
-Razlogi$ACL00[Razlogi$ACL00 == "Live performances (theatre, concerts, ballet)"] <- "Live performances"
-Razlogi$ACL00[Razlogi$ACL00 == "Cultural sites (historical monuments, museums, art galleries or archaeological sites)"] <- "Cultural sites"
+Razlogi$ACL00[Razlogi$ACL00 == "Live performances (theatre, concerts, ballet)"] <- "Nastopi v živo"
+Razlogi$ACL00[Razlogi$ACL00 == "Cultural activities (cinema, live performances or cultural sites)"] <- "Kulturne Aktivnosti"
+Razlogi$ACL00[Razlogi$ACL00 == "Cultural sites (historical monuments, museums, art galleries or archaeological sites)"] <- "Muzeji in galerije"
+Razlogi$ACL00[Razlogi$ACL00 == "Cinema"] <- "Kino"
+Razlogi$ACL00[Razlogi$ACL00 == "Sports events"] <- "Športni dogodki"
 Razlogi$FnF <- NULL 
 Razlogi$TIME <- NULL
 Razlogi$HHTYP <- NULL
 Razlogi$DEG_URB <- NULL
 Razlogi$UNIT <- NULL
-Razlogi$QUANTILE[Razlogi$QUANTILE == "First quintile"] <- 1
-Razlogi$QUANTILE[Razlogi$QUANTILE == "Second quintile"] <- 2
-Razlogi$QUANTILE[Razlogi$QUANTILE == "Third quintile"] <- 3
-Razlogi$QUANTILE[Razlogi$QUANTILE == "Fourth quintile"] <- 4
-Razlogi$QUANTILE[Razlogi$QUANTILE == "Fifth quintile"] <- 5
+Razlogi$QUANTILE[Razlogi$QUANTILE == "First quintile"] <- "1."
+Razlogi$QUANTILE[Razlogi$QUANTILE == "Second quintile"] <- "2."
+Razlogi$QUANTILE[Razlogi$QUANTILE == "Third quintile"] <- "3."
+Razlogi$QUANTILE[Razlogi$QUANTILE == "Fourth quintile"] <- "4."
+Razlogi$QUANTILE[Razlogi$QUANTILE == "Fifth quintile"] <- "5."
+Razlogi$QUANTILE[Razlogi$QUANTILE == "Total"] <- "Celotna populacija"
 Razlogi$Value1 <- Razlogi$Value
 Razlogi$Value<-NULL
 col_order <- c("GEO","ACL00", "QUANTILE","Value1","REASON")
